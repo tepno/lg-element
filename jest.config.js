139eba604806cd7f7767389e45f539275dce4a23
tepno <1379482997@qq.com>
2020-10-30 09:18:06 +0800

    打包发布

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // 运行时去哪里找测试文件
+  "testMatch": ["**/__tests__/**/*.[jt]s?(x)"],
+  "moduleFileExtensions": [
+    "js",
+    "json",
+    // 告诉 Jest 处理 `*.vue` 文件
+    "vue",
+  ],
+  "transform": {
+    // 用 `vue-jest` 处理 `*.vue` 文件
+    ".*\\.(vue)$": "vue-jest",
+    // 用 `babel-jest` 处理 `*.js` 文件
+    ".*\\.(js)$": "babel-jest"
+  }
+}
